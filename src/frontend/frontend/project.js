03cce2c099da7230f3324d5e090b9cccb2b77aa7
Charlie Wang <charlie.wang@uwaterloo.ca>
2015-09-14 11:56:41 -0400

    format

--- src/frontend/frontend/project.js
+++ src/frontend/frontend/project.js
@@ -17,54 +17,59 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 /* jshint supernew: true */
 angular.module('frontend-app')
   // Project controller.
   .controller("ProjectController", ['$state', '$stateParams', '$scope', 'error-service',
-      'openProject', '$cookies', 'NewQuestionModal', 'DeleteProjectModal',
-    function($state, $stateParams, $scope,  errors, openProject, $cookies, newQuestionModal, deleteProjectModal) {
+    'openProject', '$cookies', 'NewQuestionModal', 'DeleteProjectModal',
+    function($state, $stateParams, $scope, errors, openProject, $cookies, newQuestionModal, deleteProjectModal) {
       var self = this;
       self.state = 'edit-project';
       self.project = openProject;
       self.userid = $cookies.getObject(SEASHELL_CREDS_COOKIE).user;
-      self.download = function(){
-        openProject.getDownloadToken().then(function (token){
-            var raw = JSON.stringify(token);
-            var url = sprintf("https://%s:%s/export/%s.zip?token=%s",
-                              $cookies.getObject(SEASHELL_CREDS_COOKIE).host,
-                              $cookies.getObject(SEASHELL_CREDS_COOKIE).port,
-                              encodeURIComponent(openProject.name),
-                              encodeURIComponent(raw));
+      self.download = function() {
+        openProject.getDownloadToken().then(function(token) {
+          var raw = JSON.stringify(token);
+          var url = sprintf("https://%s:%s/export/%s.zip?token=%s",
+            $cookies.getObject(SEASHELL_CREDS_COOKIE).host,
+            $cookies.getObject(SEASHELL_CREDS_COOKIE).port,
+            encodeURIComponent(openProject.name),
+            encodeURIComponent(raw));
 
-            var ifrm = document.createElement("IFRAME");
-            ifrm.setAttribute("src", url);
-            ifrm.setAttribute("style", "display:none");
-            document.body.appendChild(ifrm);
-        });};
-        self.newQuestion = function () {
-          newQuestionModal(openProject);
-        };
-        self.close = function () {
-          $state.go('list-projects');
-        };
-        self.delete = function () {
-          deleteProjectModal(openProject.name).then(
-              function () {$state.go('list-projects');});
-        };
+          var ifrm = document.createElement("IFRAME");
+          ifrm.setAttribute("src", url);
+          ifrm.setAttribute("style", "display:none");
+          document.body.appendChild(ifrm);
+        });
+      };
+      self.newQuestion = function() {
+        newQuestionModal(openProject);
+      };
+      self.close = function() {
+        $state.go('list-projects');
+      };
+      self.delete = function() {
+        deleteProjectModal(openProject.name).then(
+          function() {
+            $state.go('list-projects');
+          });
+      };
 
       self.project.mostRecentlyUsed()
-        .then(function (recent) {
+        .then(function(recent) {
           if (recent && $state.is('edit-project')) {
-            $state.go('edit-project.editor',
-                      {question: recent},
-                      {location: "replace"});
+            $state.go('edit-project.editor', {
+              question: recent
+            }, {
+              location: "replace"
+            });
           }
           return recent;
         });
-       // Tests if project is deleteable
-       self.isDeletable = function(project) {
-            return ! /^[aA][0-9]+/.test(project);
-       };
+      // Tests if project is deleteable
+      self.isDeletable = function(project) {
+        return !/^[aA][0-9]+/.test(project);
+      };
       self.is_deleteable = self.isDeletable(self.project.name);
-    }]);
+    }
+  ]);
