af98d536baaa9e7c3bfe3db03cffc9659ee11c97
Charlie Wang <charlie.wang@uwaterloo.ca>
2015-09-22 13:52:14 -0400

    refactor project list filtering

--- src/frontend/frontend/filters.js
+++ src/frontend/frontend/filters.js
@@ -1,13 +1,20 @@
-function trimAndCompare(n) {
-  return function(a, b) {
-    // parseInt stops when it encounters non-number, so we don't need to filter trailing junk
-    var aNumeric = parseInt(a.slice(n));
-    var bNumeric = parseInt(b.slice(n));
-    // comparison function follows strcmp convention
-    return aNumeric < bNumeric ? -1 : bNumeric < aNumeric ? 1 : 0;
-  };
-}
+/*
+ * Comparison function that trims the prefix from Assignment, Lecture, and Tutorial names
+ * and provides a numeric comparison for them.
+ * Requires an assignment name of the form (A|Lec|Tut)[0-9]+
+ * @param {String} a - The first project name to compare
+ * @param {String} b - The second project name to compare
+ * @returns {Boolean} 1 if a < b; -1 if b < a; 0 if a == b
+ */
+function trimAndCompare(a, b) {
+  // parseInt stops when it encounters non-number, so we don't need to filter trailing junk
+  var aNumeric = parseInt(a.slice(a.split(/[0-9]/)[0].length));
+  var bNumeric = parseInt(b.slice(b.split(/[0-9]/)[0].length));
+  // comparison function follows strcmp convention
+  return aNumeric < bNumeric ? -1 : bNumeric < aNumeric ? 1 : 0;
 
+  // type is one of 'A', 'TUT', 'LEC', or 'NONE'
+}
 angular.module('frontend-app')
   .filter('projectFilter', function() {
     return function(input, type) {
@@ -15,30 +22,33 @@ angular.module('frontend-app')
       var pattTut = new RegExp('^Tut([0-9]+|[0-9]+Sol)$');
       var pattLec = new RegExp('^Lec[0-9]+$');
       var out = [];
+      var pattern;
+      var isNone = false;
+      switch (type) {
+        case 'A':
+          pattern = pattAssn;
+          break;
+        case 'LEC':
+          pattern = pattLec;
+          break;
+        case 'TUT':
+          pattern = pattTut;
+          break;
+        case 'NONE':
+          isNone = true;
+      }
+
       for (var i = 0; i < input.length; i++) {
-        if (type === 'A') {
-          if (pattAssn.test(input[i][0])) {
-            out.push(input[i][0]);
-          }
-        } else if (type === 'TUT') {
-          if (pattTut.test(input[i][0])) {
-            out.push(input[i][0]);
-          }
-        } else if (type === 'LEC') {
-          if (pattLec.test(input[i][0])) {
-            out.push(input[i][0]);
-          }
-        } else {
-          if (!pattAssn.test(input[i][0]) && !pattTut.test(input[i][0]) && !pattLec.test(input[i][0])) {
-            out.push(input[i][0]);
-          }
+        var name = input[i][0];
+        if (!isNone && pattern.test(name)) {
+          out.push(name);
+        } else if (isNone && !pattAssn.test(name) && !pattTut.test(name) && !pattLec.test(name)) {
+          out.push(name);
         }
       }
-      if (type === 'A') {
-        out.sort(trimAndCompare(1));
-      } else if (type === 'TUT' || type === 'LEC') {
-        out.sort(trimAndCompare(3));
-      }
+
+      if (!isNone) out.sort(trimAndCompare);
+
       return out;
     };
   });
