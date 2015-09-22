141aea0a6a004f76c2679ccbdc9e393afb893acd
Charlie Wang <charlie.wang@uwaterloo.ca>
2015-09-22 13:55:26 -0400

    re-add missing disclaimers at top of file

--- src/frontend/frontend/filters.js
+++ src/frontend/frontend/filters.js
@@ -1,3 +1,24 @@
+/**
+ * Copyright (C) 2013-2015 The Seashell Maintainers.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See also 'ADDITIONAL TERMS' at the end of the included LICENSE file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* jshint supernew: true */
+
 /*
  * Comparison function that trims the prefix from Assignment, Lecture, and Tutorial names
  * and provides a numeric comparison for them.
